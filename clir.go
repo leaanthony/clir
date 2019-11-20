4bf9ff34d390add5e2992d6f109b7c88777e38e9
Lea Anthony <lea.anthony@gmail.com>
2019-11-20 17:29:59 +1100

    support custom banner

--- clir.go
+++ clir.go
@@ -1,10 +1,19 @@
 // Package clir provides a simple API for creating command line apps
 package clir
 
+import (
+	"fmt"
+)
+
+func defaultBannerFunction(c *Cli) string {
+	return fmt.Sprintf("%s %s - %s", c.Name(), c.Version(), c.ShortDescription())
+}
+
 // NewCli - Creates a new Cli application object
 func NewCli(name, description, version string) *Cli {
 	result := &Cli{
-		version: version,
+		version:        version,
+		bannerFunction: defaultBannerFunction,
 	}
 	result.rootCommand = NewCommand(name, description)
 	result.rootCommand.setApp(result)
